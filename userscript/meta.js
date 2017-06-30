2693541938aef1fb25c6712ac65801060910b3c1
iammordaty <iammordaty@users.noreply.github.com>
2017-06-30 22:49:42 +0200

    :card_index: Updated userscript metadata

--- userscript/meta.js
+++ userscript/meta.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name SoundCloud Stream Cleaner
+// @description Userscript which allows you to hide unwanted tracks and playlists from your stream!
 // @namespace https://github.com/iammordaty
+// @author iammordaty
 // @include https://soundcloud.com/*
 // @version 0.0.1
 // @grant GM_addStyle
 // @grant GM_deleteValue
 // @grant GM_getValue
 // @grant GM_setValue
+// @homepageURL https://github.com/iammordaty/soundcloud-stream-cleaner
+// @updateURL https://raw.githubusercontent.com/iammordaty/soundcloud-stream-cleaner/master/dist/soundcloud-stream-cleaner.user.js
+// @supportURL https://github.com/iammordaty/soundcloud-stream-cleaner/issues
 // ==/UserScript==
